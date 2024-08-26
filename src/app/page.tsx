7ff37e2ce4a5f1232d1b3c3f4f2d3d546bacc9f7
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-26 17:50:05 -0300

    creating the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+// Custom components
+import Header from "@/components/Header";
+import ThemeSelector from "@/components/ThemeSelector";
+
 const HomePage: React.FC = () => {
-	return <div>HomePage</div>;
+	return <Header />;
 };
 
 export default HomePage;
