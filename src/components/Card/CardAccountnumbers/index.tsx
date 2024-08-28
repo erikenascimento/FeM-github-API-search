18eb13bf08936dff28dc67f53b7c113007c8900f
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-28 16:44:38 -0300

    Filling the accountnumbers component with API data

--- src/components/Card/CardAccountnumbers/index.tsx
+++ src/components/Card/CardAccountnumbers/index.tsx
@@ -1,20 +1,25 @@
 // Css
+import { useUser } from "@/contexts/UserContext";
 import styles from "./CardAccountnumbers.module.scss";
 
 const CardAccountnumbers = () => {
+	const { userData } = useUser();
+
 	return (
 		<section className={styles.cardAccountnumbers}>
 			<div className={styles.cardAccountnumbers__div}>
 				<span>Repos</span>
-				<data value="">0</data>
+				<data value={userData?.public_repos}>
+					{userData?.public_repos || "0"}
+				</data>
 			</div>
 			<div className={styles.cardAccountnumbers__div}>
 				<span>Followers</span>
-				<data value="">0</data>
+				<data value={userData?.followers}>{userData?.followers || "0"}</data>
 			</div>
 			<div className={styles.cardAccountnumbers__div}>
 				<span>Following</span>
-				<data value="">0</data>
+				<data value={userData?.following}>{userData?.following || "0"}</data>
 			</div>
 		</section>
 	);
