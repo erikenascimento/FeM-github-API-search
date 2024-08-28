524c84b018fba1ab17659f402d046f99b433d62f
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-28 10:00:50 -0300

    Creating new icon components

---
+++ src/components/icons/IconX/index.tsx
@@ -0,0 +1,24 @@
+const IconX = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 512 462.799"
+			preserveAspectRatio="xMidYMid meet"
+			shape-rendering="geometricPrecision"
+			text-rendering="geometricPrecision"
+			image-rendering="optimizeQuality"
+			fillRule="evenodd"
+			clipRule="evenodd"
+		>
+			<path
+				fill="var(--themed-text-color)"
+				fillRule="nonzero"
+				d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"
+			/>
+		</svg>
+	);
+};
+
+export default IconX;
