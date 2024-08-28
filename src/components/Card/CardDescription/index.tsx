169d49e2b2bd1d94fecd2f5a9139ecb480aa079d
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-28 09:11:19 -0300

    Creating component and adjusting global styles

---
+++ src/components/Card/CardDescription/index.tsx
@@ -0,0 +1,13 @@
+// Css
+import styles from "./CardDescription.module.scss";
+
+const CardDrescription = () => {
+	return (
+		<p className={styles.cardDescription}>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
+			Quisque volutpat mattis eros.
+		</p>
+	);
+};
+
+export default CardDrescription;
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,11 +2,13 @@
 import styles from "./Card.module.scss";
 // Custom components
 import CardBio from "./CardBio";
+import CardDrescription from "./CardDescription";
 
 const Card = () => {
 	return (
 		<section className={styles.card}>
 			<CardBio />
+			<CardDrescription />
 		</section>
 	);
 };
