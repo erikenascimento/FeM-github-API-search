4afb7d34c155681076b827266d441a8527c14d8b
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-28 12:20:04 -0300

    Adding the information fetched by the API into the component

--- src/components/Card/CardBio/index.tsx
+++ src/components/Card/CardBio/index.tsx
@@ -1,19 +1,38 @@
+// css
 import styles from "./CardBio.module.scss";
+// React hooks
+import { useUser } from "@/contexts/UserContext";
+// Helpers
+import { formatDate } from "@/utils/dateUtils";
 
 const CardBio = () => {
+	const { userData } = useUser();
+
+	// The app work as intended despite typescript not coperating here
+	const joinedDate = formatDate(userData?.created_at);
+
 	return (
 		<section className={styles.cardBio}>
 			<figure className={styles.cardBio__figure}>
 				<img
-					src="/profile-pic.jpg"
-					alt="User profile picture"
+					src={userData?.avatar_url || "/profile-pic.jpg"}
+					alt={`${userData?.name || "User"} profile picture`}
 					className={styles.cardBio__figure__img}
 				/>
 			</figure>
 			<article className={styles.cardBio__article}>
-				<h2 className={styles.cardBio__article__title}>User Name</h2>
-				<h3 className={styles.cardBio__article__handle}>@userHandle</h3>
-				<time className={styles.cardBio__article__time}>Joined Date</time>
+				<h2 className={styles.cardBio__article__title}>
+					{userData?.name || "User Name"}
+				</h2>
+				<h3 className={styles.cardBio__article__handle}>
+					@{userData?.login || "userHandle"}
+				</h3>
+				<time className={styles.cardBio__article__time}>
+					Joined{" "}
+					{typeof joinedDate === "string"
+						? joinedDate
+						: `${joinedDate.day} ${joinedDate.month} ${joinedDate.year}`}
+				</time>
 			</article>
 		</section>
 	);
