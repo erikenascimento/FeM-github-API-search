3b29c9f397c19887573647c7350aebd0d62b717e
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-28 18:50:51 -0300

    Now the default user will be fetched with the function in the card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,24 @@
 // Css
 import styles from "./Card.module.scss";
+// Context
+import { useUser } from "@/contexts/UserContext";
+// React hooks
+import { useEffect } from "react";
+// Custom components
 import CardAccountnumbers from "./CardAccountnumbers";
 import CardAddress from "./CardAddress";
-// Custom components
 import CardBio from "./CardBio";
 import CardDrescription from "./CardDescription";
+// Services
 
-const Card = () => {
+const Card: React.FC = () => {
+	// This will fetch the data for "erikenascimento"
+	// making it a default user
+	const { fetchUser } = useUser();
+
+	useEffect(() => {
+		fetchUser("erikenascimento");
+	}, [fetchUser]);
 	return (
 		<section className={styles.card}>
 			<CardBio />
