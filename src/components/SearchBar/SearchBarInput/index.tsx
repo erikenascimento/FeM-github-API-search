c9a9a0095bdfe4522135302ca3850402a3faf421
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-29 22:51:13 -0300

    Refactoring the input to include a state to manage the input across components and an event handler to aind on that

--- src/components/SearchBar/SearchBarInput/index.tsx
+++ src/components/SearchBar/SearchBarInput/index.tsx
@@ -1,13 +1,34 @@
 // css
+import { useState } from "react";
+// React hooks
 import styles from "./SearchBarInput.module.scss";
 
-const SearchBarInput = () => {
+//Since the username will come from the input. We will use setUsername as a prop for this component
+// Note that setUsername is a void funcion since it is an useState element
+const SearchBarInput = ({
+	setUsername,
+}: {
+	setUsername: (username: string) => void;
+}) => {
+	const [inputValue, setInputValue] = useState("");
+
+	// Here we're typing the event that will trigger as a change event of an HTML input
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		// This is for the local state
+		setInputValue(e.target.value);
+		// This is for the state from the parent component
+		setUsername(e.target.value);
+	};
+
 	return (
 		<input
 			name="SearchInput"
 			id="SearchInput"
 			className={styles.searchBarInput}
 			placeholder="Search GitHub username…"
+			value={inputValue}
+			// This will call handleChange whenever the input changes
+			onChange={handleChange}
 		></input>
 	);
 };
