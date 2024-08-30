bc9cbc7886dc39cfe7f3ef114d38f1ca9248b57f
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-29 23:07:56 -0300

    Adjusting some conditional styling that were missing

--- src/components/Card/CardAddress/index.tsx
+++ src/components/Card/CardAddress/index.tsx
@@ -11,29 +11,47 @@ import { useUser } from "@/contexts/UserContext";
 const CardAddress = () => {
 	const { userData } = useUser();
 
+	// Function bellow will either change the color of the text or do nothing depending
+	//on the return of from the API.
+	const getTextColorStyle = (data: string | null | undefined) => {
+		return data ? { color: "var(--themed-text-color)", opacity: "100%" } : {};
+	};
+
 	return (
 		<section className={styles.cardAddress}>
 			<div className={styles.cardAddress__div}>
 				<IconLocation />
-				<span className={styles.cardAddress__div__text}>
+				<span
+					className={styles.cardAddress__div__text}
+					style={getTextColorStyle(userData?.location)}
+				>
 					{userData?.location || "Not Available"}
 				</span>
 			</div>
 			<div className={styles.cardAddress__div}>
 				<IconLink />
-				<span className={styles.cardAddress__div__text}>
+				<span
+					className={styles.cardAddress__div__text}
+					style={getTextColorStyle(userData?.blog)}
+				>
 					{userData?.blog || "Not Available"}
 				</span>
 			</div>
 			<div className={styles.cardAddress__div}>
 				<IconX />
-				<span className={styles.cardAddress__div__text}>
+				<span
+					className={styles.cardAddress__div__text}
+					style={getTextColorStyle(userData?.twitter_username)}
+				>
 					{userData?.twitter_username || "Not Available"}
 				</span>
 			</div>
 			<div className={styles.cardAddress__div}>
 				<IconCompany />
-				<span className={styles.cardAddress__div__text}>
+				<span
+					className={styles.cardAddress__div__text}
+					style={getTextColorStyle(userData?.company)}
+				>
 					{userData?.company || "Not Available"}
 				</span>
 			</div>
