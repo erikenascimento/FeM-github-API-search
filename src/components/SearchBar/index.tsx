cfa9d81cb22157ccfd3b5f04c23c2f55be95219e
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-09-06 00:44:17 -0300

    Creating the error message on the interface when an user isn't found

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -14,7 +14,7 @@ const SearchBar = () => {
 	// setUsername is used on the input component
 	const [username, setUsername] = useState("");
 	// This brings from the context the function to fetch an user
-	const { fetchUser } = useUser();
+	const { userData, error, fetchUser } = useUser();
 
 	const handleSearch = () => {
 		if (username) {
@@ -28,7 +28,18 @@ const SearchBar = () => {
 				<IconSearch />
 				<SearchBarInput setUsername={setUsername} />
 			</div>
-			<Button onClick={handleSearch}>Search</Button>
+			<div className={styles.searchBar__right}>
+				{/* Conditionally display 'No results' if there's an error and no user data */}
+				{error && userData?.login === "erikenascimento" && (
+					<span
+						className={styles.searchBar__right__noResults}
+						data-testid="noResults"
+					>
+						No results
+					</span>
+				)}
+				<Button onClick={handleSearch}>Search</Button>
+			</div>
 		</section>
 	);
 };
